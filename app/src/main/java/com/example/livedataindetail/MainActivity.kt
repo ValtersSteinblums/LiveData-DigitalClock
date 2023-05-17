2d5a60b4c589f632272fe2adaa4cdfaf867cc269
ValtersSteinblums <valters.steinblums@accenture.com>
2023-05-17 13:00:02 +0300

    Counting numbers displayed on timer

--- app/src/main/java/com/example/livedataindetail/MainActivity.kt
+++ app/src/main/java/com/example/livedataindetail/MainActivity.kt
@@ -3,8 +3,10 @@ package com.example.livedataindetail
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModelProvider
 import com.example.livedataindetail.databinding.ActivityMainBinding
+import com.google.android.material.card.MaterialCardView
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,13 +21,21 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.clickMeButton.setOnClickListener {
-            viewModel.onButtonClicked()
-        }
+        bindLiveData(viewModel.segmentTopLiveData, binding.segmentTop.root)
+        bindLiveData(viewModel.segmentTopLeftLiveData, binding.segmentTopLeft.root)
+        bindLiveData(viewModel.segmentTopRightLiveData, binding.segmentTopRight.root)
+        bindLiveData(viewModel.segmentMiddleLiveData, binding.segmentMiddle.root)
+        bindLiveData(viewModel.segmentBottomLeftLiveData, binding.segmentBottomLeft.root)
+        bindLiveData(viewModel.segmentBottomRightLiveData, binding.segmentBottomRight.root)
+        bindLiveData(viewModel.segmentBottomLiveData, binding.segmentBottom.root)
 
-        viewModel.segmentStateLiveData.observe(this) {
-            binding.segment.root.apply {
-                val resolvedColor = ContextCompat.getColor(context, it)
+        viewModel.startCounting()
+    }
+
+    private fun bindLiveData(liveData: LiveData<Int>, materialCardView: MaterialCardView) {
+        liveData.observe(this) {colorRes ->
+            materialCardView.apply {
+                val resolvedColor = ContextCompat.getColor(context, colorRes)
                 setCardBackgroundColor(resolvedColor)
             }
         }
